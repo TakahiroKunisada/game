f664afa849462b92680f02241f80e8149a8e4db6
TakahiroKunisada <hw16a068@oecu.jp>
2018-03-04 15:57:12 +0900

    コミット

---
+++ Assets/Script/aaa.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class aaa : MonoBehaviour {
+
+	RaycastHit hit;
+
+	[SerializeField]
+	bool isEnable = false;
+
+	void OnDrawGizmos()
+	{
+		if (isEnable == false)
+			return;
+
+		var scale = transform.lossyScale.x * 10f;
+
+		var isHit = Physics.BoxCast (transform.position, Vector3.one * scale, transform.forward, out hit, transform.rotation);
+		if (isHit) {
+			Gizmos.DrawRay (transform.position, transform.forward * hit.distance);
+			Gizmos.DrawWireCube (transform.position + transform.forward * hit.distance, Vector3.one * scale * 2);
+		} else {
+			Gizmos.DrawRay (transform.position, transform.forward * 100);
+		}
+	}
+}
