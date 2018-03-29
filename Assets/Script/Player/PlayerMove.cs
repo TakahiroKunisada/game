0db3cfd4babe49113d31a11e6160b2dc21942c01
TakahiroKunisada <hw16a068@oecu.jp>
2018-03-29 16:00:31 +0900

    コミットしました

--- Assets/Script/Player/PlayerMove.cs
+++ Assets/Script/Player/PlayerMove.cs
@@ -43,14 +43,14 @@ public class PlayerMove : MonoBehaviour {
 //			rb.AddForce(dir *force);
 			PlayerManager.state = 1;
 			ColorReset ();
-//			Vector3 position = new Vector3( gyroscope.gravity.x * z_speed, 0, gyroscope.gravity.y * x_speed) * speed;
-//			transform.position += position;
-			float gyroScale  = 2;
+			Vector3 position = new Vector3( gyroscope.gravity.x * z_speed, 0, gyroscope.gravity.y * x_speed) * speed;
+			transform.position += position;
+//			float gyroScale  = 2;
 
-			//左右に傾けたときの重力ベクトルの値の差を左右の移動とする
-			float	h = Mathf.Clamp(Input.gyro.gravity.y * gyroScale, -1.0, 1.0);
-			//手前・奥に傾けたときの重力ベクトルの値の差を手前・奥の移動とする
-			float v = Mathf.Clamp(-1 * (Input.gyro.gravity.x - 0.6) * gyroScale, -1.0, 1.0);
+//			//左右に傾けたときの重力ベクトルの値の差を左右の移動とする
+//			float	h = Mathf.Clamp(Input.gyro.gravity.y * gyroScale, -1.0, 1.0);
+//			//手前・奥に傾けたときの重力ベクトルの値の差を手前・奥の移動とする
+//			float v = Mathf.Clamp(-1 * (Input.gyro.gravity.x - 0.6) * gyroScale, -1.0, 1.0);
 
 			}
 		//移動制限
