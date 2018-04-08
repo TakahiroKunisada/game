eee334d65766c8f16628fb9c40b8021c5bf3ce6d
TakahiroKunisada <hw16a068@oecu.jp>
2018-04-08 11:32:13 +0900

    コミット

---
+++ Assets/Script/Enemy/Enemy2.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy2 : MonoBehaviour {
+	[SerializeField]
+	[Tooltip("回転の向き")]
+	float direction;
+	[SerializeField]
+	[Tooltip("回転速度")]
+	float speed;
+	[SerializeField]
+	float width;
+	[SerializeField]
+	float height;
+	[SerializeField]
+	float pos_x;
+	[SerializeField]
+	float pos_z;
+	private float time;
+
+	// Use this for initialization
+	void Start () {
+//		InvokeRepeating ("Repeate", 1,2);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		time += Time.deltaTime * speed;
+
+
+		float x = Mathf.Cos(Time.time * speed) * width;
+		float z = Mathf.Sin(Time.time * speed) * height;
+		float y = 0f;
+		transform.position = new Vector3(x + pos_x, y, z + pos_z);
+
+		transform.position = new Vector3( Mathf.Clamp (transform.position.x, -20f, 20f),
+													   transform.position.y,
+										  Mathf.Clamp (transform.position.z, 20f, 113f));
+
+	}
+		
+	void Repeate(){
+		time += 90;
+	}
+
+}
---
+++ Assets/Script/UI/GameOver.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOver : MonoBehaviour {
+	[SerializeField]
+	GameObject gameover;
+	GameObject camera;
+	// Use this for initialization
+	void Start () {
+		camera = GameObject.Find ("Main Camera");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnTriggerEnter( Collider hit )
+	{
+		if (hit.gameObject.CompareTag ("Player")) {
+			Destroy (hit.gameObject);
+			gameover.SetActive (true);
+			camera.transform.position = new Vector3 (1,1200,69); 
+		}
+
+	}
+
+}
